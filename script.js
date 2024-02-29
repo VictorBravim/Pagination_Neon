5a8ddcc1919843b6b48907547ef1dcd49ccb4bb0
Victor Bravim <victorrafaelbravim@gmail.com>
2024-02-29 18:05:55 -0300

    Mudança

--- script.js
+++ script.js
@@ -1,310 +1,295 @@
 const FIRSTNAMES = [
-	'John', 'Steve', 'Marc', 'Franklin', 'Isaac', 'Vincent', 'Edwin',
-	'Ashlyn', 'Anthony', 'Alia', 'Abby', 'Francesca'
+    'John', 'Steve', 'Marc', 'Franklin', 'Isaac', 'Vincent', 'Edwin',
+    'Ashlyn', 'Anthony', 'Alia', 'Abby', 'Francesca'
 ];
 
 const LASTNAMES = [
-	'Shafer', 'Whitetaker', 'Glenn', 'Stephens', 'Sherman', 'Howard',
-	'Kent', 'Clay', 'Beck', 'Simmons', 'Briggs', 'Lee', 'Maxwell'
+    'Shafer', 'Whitetaker', 'Glenn', 'Stephens', 'Sherman', 'Howard',
+    'Kent', 'Clay', 'Beck', 'Simmons', 'Briggs', 'Lee', 'Maxwell'
 ];
 
 const TITLES = [
-	'CEO', 'Presidente', 'Vice Presidente', 'Diretor', 'Gerente',
-	'Desenvolvedor'
+    'CEO', 'Presidente', 'Vice Presidente', 'Diretor', 'Gerente',
+    'Desenvolvedor'
 ];
 
 const BTN_WIDTH = 4;
 
 document.addEventListener('DOMContentLoaded', () => {
-	initCopies();
-	initAutoLimits();
-	initAutoIncrements();
-	initAutoFirstnames();
-	initAutoLastnames();
-	initAutoTitles();
-	initAutoIntegers();
-	initPaginations();
+    initCopies();
+    initAutoLimits();
+    initAutoIncrements();
+    initAutoFirstnames();
+    initAutoLastnames();
+    initAutoTitles();
+    initAutoIntegers();
+    initPaginations();
 });
 
-function initCopies()
-{
-	let targets = [...document.querySelectorAll('*[data-copy]')];
+function initCopies() {
+    let targets = [...document.querySelectorAll('*[data-copy]')];
 
-	targets.reverse().forEach((original) => {
-		let amount = parseInt(original.getAttribute('data-copy'));
+    targets.reverse().forEach((original) => {
+        let amount = parseInt(original.getAttribute('data-copy'));
 
-		for (let i = 0; i < amount; i++) {
-			let copy = original.cloneNode(true);
+        for (let i = 0; i < amount; i++) {
+            let copy = original.cloneNode(true);
 
-			original.parentNode.insertBefore(copy, original.nextSibling);
-		}
-	});
+            original.parentNode.insertBefore(copy, original.nextSibling);
+        }
+    });
 }
 
-function initAutoIncrements()
-{
-	let autos = document.querySelectorAll('.auto-increment');
-	
-	autos.forEach((auto, i) => {
-		auto.innerHTML = i + 1;
-	});
+function initAutoIncrements() {
+    let autos = document.querySelectorAll('.auto-increment');
+
+    autos.forEach((auto, i) => {
+        auto.innerHTML = i + 1;
+    });
 }
 
 const rand = (a, b) => Math.floor((Math.random() * b) + a);
 
-function initAutoFirstnames()
-{
-	let autos = document.querySelectorAll('.auto-firstname');
-	
-	autos.forEach((auto) => {
-		auto.innerHTML = FIRSTNAMES[rand(0, FIRSTNAMES.length)];
-	});
+function initAutoFirstnames() {
+    let autos = document.querySelectorAll('.auto-firstname');
+
+    autos.forEach((auto) => {
+        auto.innerHTML = FIRSTNAMES[rand(0, FIRSTNAMES.length)];
+    });
 }
 
-function initAutoLastnames()
-{
-	let autos = document.querySelectorAll('.auto-lastname');
-	
-	autos.forEach((auto) => {
-		auto.innerHTML = LASTNAMES[rand(0, LASTNAMES.length)];
-	});
+function initAutoLastnames() {
+    let autos = document.querySelectorAll('.auto-lastname');
+
+    autos.forEach((auto) => {
+        auto.innerHTML = LASTNAMES[rand(0, LASTNAMES.length)];
+    });
 }
 
-function initAutoTitles()
-{
-	let autos = document.querySelectorAll('.auto-title');
-	
-	autos.forEach((auto) => {
-		auto.innerHTML = TITLES[rand(0, TITLES.length)];
-	});
+function initAutoTitles() {
+    let autos = document.querySelectorAll('.auto-title');
+
+    autos.forEach((auto) => {
+        auto.innerHTML = TITLES[rand(0, TITLES.length)];
+    });
 }
 
-function initAutoIntegers()
-{
-	let autos = document.querySelectorAll('.auto-integer');
-	
-	autos.forEach((auto) => {
-		let min = parseInt(auto.getAttribute('min'));
-		let max = parseInt(auto.getAttribute('max'));
-		
-		auto.innerHTML = rand(min, max);
-	});
+function initAutoIntegers() {
+    let autos = document.querySelectorAll('.auto-integer');
+
+    autos.forEach((auto) => {
+        let min = parseInt(auto.getAttribute('min'));
+        let max = parseInt(auto.getAttribute('max'));
+
+        auto.innerHTML = rand(min, max);
+    });
 }
 
-function initPaginations()
-{
-	let paginations = document.querySelectorAll('.pagination');
-	
-	paginations.forEach((pagination) => {
-		let table = document.getElementById(pagination.getAttribute('data-table'));
-		
-		pagination.setAttribute('data-x', '0');
+function initPaginations() {
+    let paginations = document.querySelectorAll('.pagination');
 
-		if (table !== null && table !== undefined) {
-			createPagination(pagination, table);
-			
-			pagination.closest('.table-container').addEventListener('wheel', (event) => {
-				event.preventDefault();
+    paginations.forEach((pagination) => {
+        let table = document.getElementById(pagination.getAttribute('data-table'));
 
-				scrollPages(
-					event.wheelDelta
-						? event.wheelDelta > 0
-						: event.deltaY < 0, pagination,
-					table
-				);
-			});
-		}
-	});
+        pagination.setAttribute('data-x', '0');
+
+        if (table !== null && table !== undefined) {
+            createPagination(pagination, table);
+
+            pagination.closest('.table-container').addEventListener('wheel', (event) => {
+                event.preventDefault();
+
+                scrollPages(
+                    event.wheelDelta
+                        ? event.wheelDelta > 0
+                        : event.deltaY < 0, pagination,
+                    table
+                );
+            });
+        }
+    });
 }
 
-function createPagination(pagination, table)
-{
-	let limit = parseInt(table.getAttribute("data-limit"));
-	let rows = table.querySelectorAll('.table-row:not(.table-heading)');
-	let page_count = Math.ceil(rows.length / limit);
+function createPagination(pagination, table) {
+    let limit = parseInt(table.getAttribute("data-limit"));
+    let rows = table.querySelectorAll('.table-row:not(.table-heading)');
+    let page_count = Math.ceil(rows.length / limit);
 
-	if (isNaN(limit))
-		limit = 10;
+    if (isNaN(limit))
+        limit = 10;
 
-	rows.forEach((row, index) => {
-		if (index >= limit)
-			row.style.display = 'none';
-	});
-	
-	for (let i = 0; i < page_count; i++) {
-		let new_button = document.createElement('li');
+    rows.forEach((row, index) => {
+        if (index >= limit)
+            row.style.display = 'none';
+    });
 
-		new_button.innerHTML = "<span>" + (i + 1) + "</span>";
-		
-		if (i === 0)
-			new_button.classList.add('active');
+    for (let i = 0; i < page_count; i++) {
+        let new_button = document.createElement('li');
 
-		pagination.appendChild(new_button);
-		
-		new_button.addEventListener('click', () => {
-			switchPage(pagination, table, i);
-		});
-	}
+        new_button.innerHTML = "<span>" + (i + 1) + "</span>";
 
-	updatePaginationInfos(pagination, table, page_count, 0);
-	initPaginationExtremes(pagination, table, page_count);
+        if (i === 0)
+            new_button.classList.add('active');
+
+        pagination.appendChild(new_button);
+
+        new_button.addEventListener('click', () => {
+            switchPage(pagination, table, i);
+        });
+    }
+
+    updatePaginationInfos(pagination, table, page_count, 0);
+    initPaginationExtremes(pagination, table, page_count);
 }
 
-function scrollPages(direction, pagination, table)
-{
-	let last_active = pagination.querySelector('li.active');
-	let buttons = pagination.querySelectorAll('li');
-	let last_index = Array.from(buttons).indexOf(last_active);
-	let scroll_index = null;
-	
-	if (direction && last_index > 0) {
-		scroll_index = last_index - 1;
-	} else if (!direction && last_index < buttons.length - 1) {
-		scroll_index = last_index + 1;
-	}
+function scrollPages(direction, pagination, table) {
+    let last_active = pagination.querySelector('li.active');
+    let buttons = pagination.querySelectorAll('li');
+    let last_index = Array.from(buttons).indexOf(last_active);
+    let scroll_index = null;
 
-	if (scroll_index !== null)
-		switchPage(pagination, table, scroll_index);
+    if (direction && last_index > 0) {
+        scroll_index = last_index - 1;
+    } else if (!direction && last_index < buttons.length - 1) {
+        scroll_index = last_index + 1;
+    }
+
+    if (scroll_index !== null)
+        switchPage(pagination, table, scroll_index);
 }
 
-function switchPage(pagination, table, index, bypass = -1)
-{
-	let limit = parseInt(table.getAttribute("data-limit"));
-	let rows = table.querySelectorAll('.table-row:not(.table-heading)');
-	let buttons = pagination.querySelectorAll('li');
-	let last_active = pagination.querySelector('li.active');
-	let x_pos = parseInt(pagination.getAttribute('data-x'));
-	let x_shift = 0;
-	let target_pos = (-index + 2) * BTN_WIDTH;
-	let page_count = Math.ceil(rows.length / limit);
+function switchPage(pagination, table, index, bypass = -1) {
+    let limit = parseInt(table.getAttribute("data-limit"));
+    let rows = table.querySelectorAll('.table-row:not(.table-heading)');
+    let buttons = pagination.querySelectorAll('li');
+    let last_active = pagination.querySelector('li.active');
+    let x_pos = parseInt(pagination.getAttribute('data-x'));
+    let x_shift = 0;
+    let target_pos = (-index + 2) * BTN_WIDTH;
+    let page_count = Math.ceil(rows.length / limit);
 
-	if (bypass !== -1) {
-		x_shift = (-bypass + 2) * BTN_WIDTH;
-	} else {
-		if (index > 1 && index < buttons.length - 2) {
-			x_shift = target_pos;
-		} else if (index === 1) {
-			x_shift = 0;
-		} else if (index === page_count - 2) {
-			x_shift = (-page_count + 5) * BTN_WIDTH;
-		} else {
-			x_shift = x_pos;
-		}
-	}
-	
-	rows.forEach((row, row_index) => {
-		if (row_index < index * limit || row_index >= (index * limit) + limit) {
-			row.style.display = 'none';
-		} else {
-			row.style.display = 'flex';
-			row.style.opacity = '0';
-			
-			setTimeout(() => {
-				row.style.opacity = '1';
-			}, 50);
-		}
-	});
+    if (bypass !== -1) {
+        x_shift = (-bypass + 2) * BTN_WIDTH;
+    } else {
+        if (index > 1 && index < buttons.length - 2) {
+            x_shift = target_pos;
+        } else if (index === 1) {
+            x_shift = 0;
+        } else if (index === page_count - 2) {
+            x_shift = (-page_count + 5) * BTN_WIDTH;
+        } else {
+            x_shift = x_pos;
+        }
+    }
 
-	last_active.classList.remove('active');
-	buttons[index].classList.add('active');
-	pagination.style.transform = 'translateX(' + x_shift + 'rem)';
-	pagination.setAttribute('data-x', x_shift);
+    rows.forEach((row, row_index) => {
+        if (row_index < index * limit || row_index >= (index * limit) + limit) {
+            row.style.display = 'none';
+        } else {
+            row.style.display = 'flex';
+            row.style.opacity = '0';
 
-	updatePaginationInfos(pagination, table, page_count, index);
-	updatePaginationProgress(pagination, index, page_count - 1);
+            setTimeout(() => {
+                row.style.opacity = '1';
+            }, 50);
+        }
+    });
+
+    last_active.classList.remove('active');
+    buttons[index].classList.add('active');
+    pagination.style.transform = 'translateX(' + x_shift + 'rem)';
+    pagination.setAttribute('data-x', x_shift);
+
+    updatePaginationInfos(pagination, table, page_count, index);
+    updatePaginationProgress(pagination, index, page_count - 1);
 }
 
-function updatePaginationInfos(pagination, table, page_count, index)
-{
-	let info = pagination.closest('.pagination-container').querySelector('.pagination-info');
-	
-	if (info === null || info === undefined)
-		return;
+function updatePaginationInfos(pagination, table, page_count, index) {
+    let info = pagination.closest('.pagination-container').querySelector('.pagination-info');
 
-	let from = 0, to = 0;
-	let rows = table.querySelectorAll('.table-row:not(.table-heading)');
-	let started = false;
-	
-	for (let i = 1; i < rows.length; i++) {
-		let display = rows[i - 1].style.display;
-		
-		if (display !== 'none' && !started) {
-			started = true;
-			from = i;
-		} else if ((display === 'none' && started) || i == rows.length - 1) {
-			to = i == rows.length - 1 ? rows.length : i - 1;
-			break;
-		}
-	}
-	
-	info.innerHTML = 'Exibindo' + from + '-' + to + ' na página ' + (index + 1) + '/' + page_count;
+    if (info === null || info === undefined)
+        return;
+
+    let from = 0, to = 0;
+    let rows = table.querySelectorAll('.table-row:not(.table-heading)');
+    let started = false;
+
+    for (let i = 1; i < rows.length; i++) {
+        let display = rows[i - 1].style.display;
+
+        if (display !== 'none' && !started) {
+            started = true;
+            from = i;
+        } else if ((display === 'none' && started) || i == rows.length - 1) {
+            to = i == rows.length - 1 ? rows.length : i - 1;
+            break;
+        }
+    }
+
+    info.innerHTML = 'Exibindo' + from + '-' + to + ' na página ' + (index + 1) + '/' + page_count;
 }
 
-function initPaginationExtremes(pagination, table, max)
-{
-	let container = pagination.closest('.pagination-container');
-	let left = container.querySelector('.pagination-left');
-	let right = container.querySelector('.pagination-right');
+function initPaginationExtremes(pagination, table, max) {
+    let container = pagination.closest('.pagination-container');
+    let left = container.querySelector('.pagination-left');
+    let right = container.querySelector('.pagination-right');
 
-	if (left !== null && left !== undefined) {
-		left.addEventListener('click', () => {
-			switchPage(pagination, table, 0, Math.min(2, max - 1));
-		});
-	}
+    if (left !== null && left !== undefined) {
+        left.addEventListener('click', () => {
+            switchPage(pagination, table, 0, Math.min(2, max - 1));
+        });
+    }
 
-	if (right !== null && right !== undefined) {
-		right.addEventListener('click', () => {
-			switchPage(pagination, table, max - 1, Math.max(0, max - 3));
-		});
-	}
+    if (right !== null && right !== undefined) {
+        right.addEventListener('click', () => {
+            switchPage(pagination, table, max - 1, Math.max(0, max - 3));
+        });
+    }
 
-	initPaginationSteppedExtremes(pagination, table, container, max);
+    initPaginationSteppedExtremes(pagination, table, container, max);
 }
 
-function initPaginationSteppedExtremes(pagination, table, container, max)
-{
-	let left = container.querySelector('.pagination-left-one');
-	let right = container.querySelector('.pagination-right-one');
-	
-	if (left !== null && left !== undefined) {
-		left.addEventListener('click', () => {
-			let last_active = pagination.querySelector('li.active');
-			let buttons = pagination.querySelectorAll('li');
-			let last_index = Array.from(buttons).indexOf(last_active);
-			
-			if (last_index > 0)
-				switchPage(pagination, table, last_index - 1);
-		});
-	}
-	
-	if (right !== null && right !== undefined) {
-		right.addEventListener('click', () => {
-			let last_active = pagination.querySelector('li.active');
-			let buttons = pagination.querySelectorAll('li');
-			let last_index = Array.from(buttons).indexOf(last_active);
-			
-			if (last_index < max - 1)
-				switchPage(pagination, table, last_index + 1);
-		});
-	}
+function initPaginationSteppedExtremes(pagination, table, container, max) {
+    let left = container.querySelector('.pagination-left-one');
+    let right = container.querySelector('.pagination-right-one');
+
+    if (left !== null && left !== undefined) {
+        left.addEventListener('click', () => {
+            let last_active = pagination.querySelector('li.active');
+            let buttons = pagination.querySelectorAll('li');
+            let last_index = Array.from(buttons).indexOf(last_active);
+
+            if (last_index > 0)
+                switchPage(pagination, table, last_index - 1);
+        });
+    }
+
+    if (right !== null && right !== undefined) {
+        right.addEventListener('click', () => {
+            let last_active = pagination.querySelector('li.active');
+            let buttons = pagination.querySelectorAll('li');
+            let last_index = Array.from(buttons).indexOf(last_active);
+
+            if (last_index < max - 1)
+                switchPage(pagination, table, last_index + 1);
+        });
+    }
 }
 
-function updatePaginationProgress(pagination, index, total)
-{
-	let dot = pagination.closest('.table-container').querySelector('.progress-point');
-	
-	dot.style.left = ((index / total) * 100) + '%';
+function updatePaginationProgress(pagination, index, total) {
+    let dot = pagination.closest('.table-container').querySelector('.progress-point');
+
+    dot.style.left = ((index / total) * 100) + '%';
 }
 
-function initAutoLimits()
-{
-	let table_containers = document.querySelectorAll('.table-container');
-	let limit = window.innerHeight / 70;
-	
-	table_containers.forEach((container) => {
-		let table = container.querySelector('.table');
+function initAutoLimits() {
+    let table_containers = document.querySelectorAll('.table-container');
+    let limit = window.innerHeight / 70;
 
-		container.setAttribute('style', '--data-limit: ' + limit);
-		table.setAttribute('data-limit', limit);
-	});
+    table_containers.forEach((container) => {
+        let table = container.querySelector('.table');
+
+        container.setAttribute('style', '--data-limit: ' + limit);
+        table.setAttribute('data-limit', limit);
+    });
 }
